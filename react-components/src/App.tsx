b7ee8ae92e717654a7af839cfdf459384e49f9e2
Recomagnesius <buzhor.danyl@gmail.com>
2023-03-04 02:44:47 -0600

    init: create template and add routing

---
+++ react-components/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import React from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import Home from "./components/Home";
+  
+function App() {
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home/> } />
+        <Route path='/about' element={ <h1>ABOUT</h1> } />
+        <Route path="/404" element={<h1>PageNotFound</h1>} />
+        <Route path="*" element={<Navigate to="/404" />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
